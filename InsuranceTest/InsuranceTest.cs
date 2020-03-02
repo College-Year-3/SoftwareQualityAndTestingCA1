ea7f9d7a25405d75ef73cf50cf1ba6a702c195dc
s00083446 <s00083446@mail.itsligo.ie>
2020-03-02 07:57:22 +0000

    Projects created and packages installed

---
+++ InsuranceTest/InsuranceTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication2;
+
+using static WebApplication2._Default;
+
+namespace InsuranceTest 
+{
+    public class InsuranceTest //: ColumnFixture
+    {
+        public static InsuranceService SUT;
+        public static int  vehicleValue;
+        public static string comp3rd;
+        public static DateTime dob;
+        public static int penPoin;
+
+        public float premium()
+        {
+            SUT = new InsuranceService();
+            return SUT.CalcPremium(vehicleValue, comp3rd, dob, penPoin);
+        }
+
+    }
+}
---
+++ WebApplication2/Default.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services.Description;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class _Default : Page
+    {
+        public InsuranceService Service = new InsuranceService();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int vehicleValue = int.Parse(VehichleVal.Text);
+            string comp3rd = Comp3rd.Text;
+            DateTime dob = DateTime.Parse(DOB.Text);
+            int penPoin = int.Parse(PenP.Text);
+
+
+
+
+            float premium = Service.CalcPremium(vehicleValue, comp3rd, dob, penPoin);
+
+            ResultLab.Text = premium.ToString();
+        }
+
+        public class InsuranceService
+        {
+
+            public float CalcPremium(int age, string comp3rd, DateTime dob, int penPoin)
+            {
+                float premium = 0;
+
+                return premium;
+            }
+        }
+    }
+}
